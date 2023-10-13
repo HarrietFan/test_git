cd144479f310215a120756f5960099d07ef67496
duhaifan <dhf@qq.com>
2023-10-13 20:56:25 +0800

    第二次提交

--- com/coder/Test.java
+++ com/coder/Test.java
@@ -4,5 +4,7 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("Hello, Java and Git");
         System.out.println("HelloAAA");
+        System.out.println("HelloAAA");
+
     }
 }
