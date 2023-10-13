d5d08b90d90d4f57107cdb38a1ed3fadcdd5348d
duhaifan <dhf@qq.com>
2023-10-13 20:52:48 +0800

    第一次提交

---
+++ com/coder/Test.java
@@ -0,0 +1,8 @@
+package com.coder;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello, Java and Git");
+        System.out.println("HelloAAA");
+    }
+}
