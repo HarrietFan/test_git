8b4448218b8ec5c2d9e9c6169422a5a50892dbf9
Ariel <118145800+HarrietFan@users.noreply.github.com>
2023-10-14 19:40:00 +0800

    Update Test.java

--- com/coder/Test.java
+++ com/coder/Test.java
@@ -9,5 +9,6 @@ public class Test {
         System.out.println("Hello, Java and Git");
         System.out.println("Hello, dev branch");
         System.out.println("Hello, fix branch");
+        System.out.println("这是一项来自idea集成git项目的文件，本段文字来源于github远程仓库。");
     }
 }
